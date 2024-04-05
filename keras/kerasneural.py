7e1554dcb538f97c2b0cd2c27224f0fb321adc5b
natatchikhinashvili <natanatuka33@gmail.com>
2024-04-05 13:40:57 +0400

    feat: change test size tp 30 precent

--- keras/kerasneural.py
+++ keras/kerasneural.py
@@ -6,4 +6,4 @@ df = read_csv('../basic/iris.csv')
 X = df.drop(columns=["variety"])
 y = df["variety"]
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.3, random_state=42)
